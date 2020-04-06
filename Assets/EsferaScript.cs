860a6d3015720a5d22205f19835fafb734af4e01
Bruno Pessoa <brunopessoa.melo@gmail.com>
2020-04-05 21:02:21 -0300

    adicionando física na esfera, testando fisica de Trigger

---
+++ Assets/EsferaScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EsferaScript : MonoBehaviour {
+
+	Rigidbody m_rigibody;
+
+	void Start () {
+		m_rigibody = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+		
+			m_rigibody.AddForce (new Vector3(0, 100, 0));
+		
+		}
+	
+	}
+}
